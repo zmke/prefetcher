6dabb204f8bd0260eaebbb606bac8ccb0a9289a4
zmke <ppo1996a@gmail.com>
2017-03-18 22:05:33 +0800

    Encapsulate different matrix transpose methods

--- main.c
+++ main.c
@@ -16,6 +16,35 @@
 
 #include "impl.c"
 
+typedef void (*transpose_t) (int *src, int *dst, int w, int h);
+typedef struct __MATRIX_T {
+    transpose_t transpose;
+} matrix_t;
+
+int init_naive (matrix_t **self)
+{
+    if (NULL == (*self = malloc(sizeof(matrix_t))))
+        return -1;
+    (*self)->transpose = naive_transpose;
+    return 0;
+}
+
+int init_sse (matrix_t **self)
+{
+    if (NULL == (*self = malloc(sizeof(matrix_t))))
+        return -1;
+    (*self)->transpose = sse_transpose;
+    return 0;
+}
+
+int init_sse_prefetch (matrix_t **self)
+{
+    if (NULL == (*self = malloc(sizeof(matrix_t))))
+        return -1;
+    (*self)->transpose = sse_prefetch_transpose;
+    return 0;
+}
+
 static long diff_in_us(struct timespec t1, struct timespec t2)
 {
     struct timespec diff;
@@ -31,64 +60,37 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
 
 int main()
 {
-    /* verify the result of 4x4 matrix */
-    {
-        int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
-                           8, 9, 10, 11, 12, 13, 14, 15
-                         };
-        int testout[16];
-        int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
-                             2, 6, 10, 14, 3, 7, 11, 15
-                           };
 
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testin[y * 4 + x]);
-            printf("\n");
-        }
-        printf("\n");
-        sse_transpose(testin, testout, 4, 4);
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testout[y * 4 + x]);
-            printf("\n");
-        }
-        assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
-               "Verification fails");
-    }
+    struct timespec start, end;
+    int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    matrix_t *matrix_transpose	= NULL;
 
-    {
-        struct timespec start, end;
-        int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    srand(time(NULL));
+    for (int y = 0; y < TEST_H; y++)
+        for (int x = 0; x < TEST_W; x++)
+            *(src + y * TEST_W + x) = rand();
 
-        srand(time(NULL));
-        for (int y = 0; y < TEST_H; y++)
-            for (int x = 0; x < TEST_W; x++)
-                *(src + y * TEST_W + x) = rand();
+#ifdef NAIVE
+    init_naive(&matrix_transpose);
+#endif
 
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
+#ifdef SSE
+    init_sse(&matrix_transpose);
+#endif
 
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_transpose(src, out1, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse: \t\t %ld us\n", diff_in_us(start, end));
+#ifdef SSE_PREFETCH
+    init_sse_prefetch(&matrix_transpose);
+#endif
 
-        clock_gettime(CLOCK_REALTIME, &start);
-        naive_transpose(src, out2, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("naive: \t\t %ld us\n", diff_in_us(start, end));
 
-        free(src);
-        free(out0);
-        free(out1);
-        free(out2);
-    }
+    int *out = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    clock_gettime(CLOCK_REALTIME, &start);
+    matrix_transpose->transpose(src, out, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("Execution time: \t %ld us\n", diff_in_us(start, end));
+    free(out);
+    free(src);
+
 
     return 0;
 }
